69711ce9e0e6eb368cc8cc4515fc3330c35cfda5
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-09 19:22:58 +0700

    CT sd Hàm tính BMI

--- TinhBMI/Program.cs
+++ TinhBMI/Program.cs
@@ -12,25 +12,28 @@ namespace TinhBMI
             double chieucao = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Nhap can nang (kg):");
             double cannang = Convert.ToDouble(Console.ReadLine());
+            TinhBMI(chieucao, cannang);
+        }
+        static double TinhBMI(Double chieucao ,double cannang)
+        {
             double bmi = cannang / (chieucao * chieucao);
             if (bmi < 18.5)
             {
-                Console.WriteLine("BMI = {0} Ban la nguoi gay !",bmi);
+                Console.WriteLine("BMI = {0} Ban la nguoi gay !", bmi);
             }
-            else if ((18.5 <= bmi ) && (bmi < 25))
+            else if ((18.5 <= bmi) && (bmi < 25))
             {
-                Console.WriteLine("BMI = {0} Ban la nguoi binh thuong !",bmi);
+                Console.WriteLine("BMI = {0} Ban la nguoi binh thuong !", bmi);
             }
             else if ((25 <= bmi) && (bmi < 30))
             {
-                Console.WriteLine("BMI = {0} Ban la nguoi thua can !",bmi);
+                Console.WriteLine("BMI = {0} Ban la nguoi thua can !", bmi);
             }
             else
             {
-                Console.WriteLine("BMI = {0} Ban la nguoi beo phi !",bmi);
+                Console.WriteLine("BMI = {0} Ban la nguoi beo phi !", bmi);
             }
-
-
+            return 0;
         }
     }
 }
